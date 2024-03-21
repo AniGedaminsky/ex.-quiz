257ede49fe0d1599434f0beb94a70187c20248d2
AniGedaminsky <160894666+AniGedaminsky@users.noreply.github.com>
2024-03-21 19:09:00 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+//function submitAnswer() {
+    //const correctAnswer = "Hyper Text MarkUp Language";
+    //let feedback = document.getElementById("feedback");
+    //let selected = document.querySelector('input[name="quiz"]:checked');
+
+    //if (selected && selected.value === correctAnswer) {
+      //  feedback.textContent = "Correct! Well done.";
+        //feedback.style.color = "green";
+   // } else {
+     //   feedback.textContent = "Incorrect. Try again!";
+       // feedback.style.color = "red";
+   // }
+//} 
+function submitAnswer() {
+    const correctAnswer = "Hyper Text MarkUp Language";
+    let feedback = document.getElementById("feedback");
+    let selected = document.querySelector('input[name="quiz"]:checked');
+
+    if (selected && selected.value === correctAnswer) {
+        feedback.textContent = "Correct! Well done.";
+        feedback.style.color = "green";
+    }else {
+        feedback.textContent = "Incorrect Try Again!";
+        feedback.style.color = "red";
+    }
+ }
